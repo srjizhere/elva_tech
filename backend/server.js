bccd1959924527f7e9fac2f61cf9704058b997a4
srjizhere <bhiapatil@gmail.com>
2023-07-22 13:57:45 +0530

    companySchema define

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
 const express = require("express");
-
+const cors = require('cors')
 require("dotenv").config();
 const app = express();
 
 const { connection } = require("./config/db");
-app.use(express.json());
+const { CompanyModel } = require("./model/company.model");
 
+
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 7000;
 
 app.get("/", (req, res) => {
   res.send({ msg: "this is base route for Eleva-Tech assignment" });
 });
-
-
 
 
 const startServer = async () => {
